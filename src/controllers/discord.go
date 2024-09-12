a1545b6489c588ddb9bf1e0ab232be3585c10337
Jack Nash <jacknash42@gmail.com>
2024-09-13 00:55:41 +1000

    fix: fix unexpected message fetching error

--- src/controllers/discord.go
+++ src/controllers/discord.go
@@ -119,47 +119,84 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 }
 
 func threadCreate(s *discordgo.Session, t *discordgo.ThreadCreate) {
-	if t.ID != discordContext.channelId {
+	if t.ParentID != discordContext.channelId {
 		return
 	}
+
 	if t.NewlyCreated {
-		message_id := t.LastMessageID
-		message, err := s.ChannelMessage(t.ID, message_id)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		var header = fmt.Sprintf("> Posted by **@%s**\n\n", message.Author.Username)
-		var content = fmt.Sprintf("%s%s", header, message.Content)
-
-		var issue, create_err = CreateIssue(CreateIssueRequest{
-			Title:  t.Name,
-			Body:   content,
-			Labels: []string{},
-			Headers: Headers{
-				XGitHubApiVersion: "2022-11-28",
-			},
-			Owner: githubContext.author,
-			Repo:  githubContext.repo,
-		})
-
-		if create_err != nil {
-			s.ChannelMessageSend(t.ID, create_err.Error())
-			return
-		}
-
-		var threadRename = fmt.Sprintf("%d) %s", issue.Number, t.Name)
-		_, err = s.ChannelEdit(t.ID, &discordgo.ChannelEdit{
-			Name: threadRename,
-		})
-
-		if err != nil {
-			s.ChannelMessageSend(t.ID, fmt.Sprintf("Failed to rename thread: %s", err.Error()))
-			return
-		}
-
-		var response = fmt.Sprintf("[Issue #%d created](<%s>)", issue.Number, issue.HtmlUrl)
-		s.ChannelMessageSend(t.ID, response)
+		createIssueForThread(s, t)
 	}
+}
+
+func getAllMessagesFromThread(s *discordgo.Session, threadID string) ([]*discordgo.Message, error) {
+	var allMessages []*discordgo.Message
+	var lastMessageID string
+
+	for {
+		messages, err := s.ChannelMessages(threadID, 5, lastMessageID, "", "")
+		if err != nil {
+			return nil, err
+		}
+
+		if len(messages) == 0 {
+			break
+		}
+		allMessages = append(allMessages, messages...)
+		lastMessageID = messages[len(messages)-1].ID
+	}
+
+	return allMessages, nil
+}
+
+func createIssueForThread(s *discordgo.Session, t *discordgo.ThreadCreate) {
+	messages, err := getAllMessagesFromThread(s, t.ID)
+	if err != nil {
+		fmt.Printf("Message Err: %s", err)
+		return
+	}
+	if len(messages) == 0 {
+		fmt.Printf("Message Err: No Messages")
+		return
+	}
+
+	oldestMessage := messages[0]
+	for _, message := range messages {
+		if message.Timestamp.Before(oldestMessage.Timestamp) {
+			oldestMessage = message
+		}
+	}
+
+	var header = fmt.Sprintf("> Posted by **@%s**\n\n", oldestMessage.Author.Username)
+	var content = fmt.Sprintf("%s%s", header, oldestMessage.Content)
+
+	var issue, create_err = CreateIssue(CreateIssueRequest{
+		Title:  t.Name,
+		Body:   content,
+		Labels: []string{},
+		Headers: Headers{
+			XGitHubApiVersion: "2022-11-28",
+		},
+		Owner: githubContext.author,
+		Repo:  githubContext.repo,
+	})
+
+	if create_err != nil {
+		fmt.Printf("Issue Creation Err: %s", create_err.Error())
+		s.ChannelMessageSend(t.ID, create_err.Error())
+		return
+	}
+
+	var threadRename = fmt.Sprintf("%d) %s", issue.Number, t.Name)
+	_, err = s.ChannelEdit(t.ID, &discordgo.ChannelEdit{
+		Name: threadRename,
+	})
+
+	if err != nil {
+		fmt.Printf("Thread Rename Err: %s", err.Error())
+		s.ChannelMessageSend(t.ID, fmt.Sprintf("Failed to rename thread: %s", err.Error()))
+		return
+	}
+
+	var response = fmt.Sprintf("[Issue #%d created](<%s>)", issue.Number, issue.HtmlUrl)
+	s.ChannelMessageSend(t.ID, response)
 }
