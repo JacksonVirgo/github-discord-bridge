f6aa527c149d770f8c9aeeceba067773bdca3250
Jack Nash <jacknash42@gmail.com>
2024-09-12 11:29:31 +1000

    feat: add dotenv

--- src/main.go
+++ src/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 
 func main() {
-	fmt.Println("Hello, World!")
+	err := godotenv.Load()
+	if err != nil {
+    	log.Fatal("Error loading .env file")
+  	}
+
+	discordToken := os.Getenv("DISCORD_TOKEN")
+	fmt.Printf("Discord token: %s\n", discordToken)
 }
