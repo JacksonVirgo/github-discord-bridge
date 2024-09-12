f53ed21970e705560eea85b5afa7a4e5c667fc8b
Jack Nash <jacknash42@gmail.com>
2024-09-12 21:50:28 +1000

    fix: fix memory issue with github context

--- src/controllers/github.go
+++ src/controllers/github.go
@@ -2,6 +2,9 @@ package controllers
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
 
@@ -12,13 +15,14 @@ type GithubContext struct {
 	author string
 }
 
-var githubContext *GithubContext
+var githubContext = &GithubContext{}
 
 func LoadGithubContext() error {
 	githubToken := os.Getenv("GITHUB_TOKEN")
 	repo := os.Getenv("GITHUB_REPO")
 	author := os.Getenv("GITHUB_AUTHOR")
 
+	fmt.Printf("Loaded Github Context: %v, %v, %v\n", githubToken, repo, author)
 	if githubToken == "" || repo == "" || author == "" {
 		return errors.New("missing environment variables")
 	}
@@ -30,4 +34,29 @@ func LoadGithubContext() error {
 	}
 
 	return nil
+}
+
+func GetIssues() (string, error) {
+    url := fmt.Sprintf("https://api.github.com/repos/%s/%s/issues", githubContext.author, githubContext.repo)
+	req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+		return "", err
+    }
+
+	req.Header.Set("Authorization", "token "+githubContext.token)
+    req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
+
+	client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+		return "", err
+    }
+    defer resp.Body.Close()
+
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+		return "", err
+    }
+
+	return string(body), nil
 }
