ed61b1fb9f351559912b91b8e5f895e5f5108a6d
Jack Nash <jacknash42@gmail.com>
2024-09-13 05:24:23 +1000

    feat: add getIssueLabels

---
+++ src/github/issues/labels.go
@@ -0,0 +1,60 @@
+package issues
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/JacksonVirgo/github-discord-bridge/src/github"
+)
+
+type Label struct {
+	Name string `json:"name"`
+}
+
+func GetIssueLabels() ([]string, error) {
+	token := github.GetToken()
+	repo := github.GetRepo()
+	author := github.GetAuthor()
+
+	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/issues", author, repo)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Printf("Error creating request: %s", err)
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("token %s", token))
+	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("Error sending request: %s", err)
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Error reading response body: %s", err)
+		return nil, err
+	}
+
+	var issues []Label
+	err = json.Unmarshal(body, &issues)
+	if err != nil {
+		log.Printf("Error parsing JSON: %s", err)
+		return nil, err
+	}
+
+	var labels []string
+	for _, issue := range issues {
+		labels = append(labels, issue.Name)
+	}
+
+	return labels, nil
+}
